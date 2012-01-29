0c580c3ad8c2a2b89a68d5f1d5dd02b169da4d40
Andrew Plummer <plummer.andrew@gmail.com>
2012-01-30 01:30:55 +0900

    more

--- test/unit.js
+++ test/unit.js
@@ -37,100 +37,20 @@
     equal(api.domain(), 'test', ' APIInterface#domain calling without arguments should return the field');
   });
 
-  // Plural:
+  test('Custom Setters', function() {
 
-  // GET    /users
-  // GET    /users/4
-  // POST   /users
-  // PUT    /users/4
-  // DELETE /users/4
+    api.protocol('https');
+    api.domain('twotter.com');
+    api.port('5000');
+    api.format('xml');
 
-  // Singular:
+    api.get('foobar');
+    api.getFoobar();
 
-  // GET    tweet/user
-  // POST   tweet/user
-  // PUT    tweet/user
-  // DELETE tweet/user
+    assertRouteCalled(api, 'https://twotter.com:5000/foobar.xml', 'GET')
 
-
-
-  //  api.get
-
-
-
-
-
-  /*
-
-  api.context('foobar', function() {
-    api.resource('mice');
-    api.resource('foo');
-    api.context('mow', function() {
-      api.resource('mama');
-    });
   });
 
-  api.context('statuses', function() {
-    api.get('home_timeline');
-    api.get('mentions');
-  });
-
-
-  api.get('home_timeline', { prefix: 'statuses' });
-
-  api.get('mentions', { context: 'status' });
-
-  api.get('items', { context: 'goal' });
-
-  api.get('sentences', { context: 'goal' });
-  api.get('sentences', { context: ['goal','item'] });
-
-
-
-
-  GET :user/lists/memberships
-
-  GET lists/memberships
-
-
-  getUserListsMemberships()
-
-  getListsMemberships()
-
-
-  GET lists/members
-  DELETE :user/:list_id/members
-  GET :user/:list_id/members
-  GET :user/:list_id/members/:id
-  POST :user/:list_id/members
-
-
-  getListMembers()
-  deleteUserListMembers()
-  getUserListMembers()
-  getUserListMembers()
-  postUserListMembers()
-
-
-  GET statuses/home_timeline
-
-
-  getStatusHomeTimeline();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  */
 
   test('get | home_timeline', function() {
     api.get('home_timeline');
@@ -586,16 +506,26 @@
   });
 
 
+  test('Deep contexts are properly set', function() {
+
+    api.context('neighborhoods/:neighborhood_id', function() {
+      api.context('homes/:home_id', function() {
+        equal(api.base.length, 4, 'Context should have base length of 4');
+      });
+    });
+
+  });
+
   test('Multiple contexts on different levels', function() {
 
     api.context('neighborhoods/:neighborhood_id', function() {
       api.get('cats');
     });
-    //api.context('neighborhoods/:neighborhood_id', function() {
-      //api.context('homes/:home_id', function() {
-        //api.get('cats');
-      //});
-    //});
+    api.context('neighborhoods/:neighborhood_id', function() {
+      api.context('homes/:home_id', function() {
+        api.get('cats');
+      });
+    });
 
     api.get('cats');
 
@@ -605,164 +535,14 @@
     api.getCats({ neighborhood_id: 5 });
     assertRouteCalled(api, 'http://domain/neighborhoods/5/cats.json', 'GET');
 
-    //api.getCats({ home_id: 5 });
-    //assertRouteCalled(api, 'http://domain/homes/5/cats.json', 'GET');
+    api.getCats({ home_id: 5 });
+    assertRouteCalled(api, 'http://domain/homes/5/cats.json', 'GET');
 
-    //api.getCats({ neighborhood_id: 18, home_id: 5 });
-    //assertRouteCalled(api, 'http://domain/neighborhoods/18/homes/5/cats.json', 'GET');
+    api.getCats({ neighborhood_id: 18, home_id: 5 });
+    assertRouteCalled(api, 'http://domain/neighborhoods/18/homes/5/cats.json', 'GET');
 
 
   });
 
-
-
-
-
-
-
-
-
-
-  /*
-
-  test('show | with prefix', function() {
-    api.show('home_timeline', { prefix: 'statuses' });
-    api.getHomeTimeline();
-    assertRouteCalled(api, 'http://domain/statuses/home_timeline.json', 'GET')
-  });
-
-  test('show | with multiple prefixes', function() {
-    api.show('home_timeline', { prefix: 'status,matus' });
-    api.getHomeTimeline();
-    assertRouteCalled(api, 'http://domain/status/matus/home_timeline.json', 'GET')
-
-  });
-
-  test('show | with unused context', function() {
-    api.show('retweeted_by', { context: 'status' });
-    api.getRetweetedBy();
-    assertRouteCalled(api, 'http://domain/retweeted_by.json', 'GET')
-  });
-
-  test('show | with used context', function() {
-    api.show('retweeted_by', { context: 'status' });
-    api.getRetweetedBy({ status_id: 3 });
-    assertRouteCalled(api, 'http://domain/statuses/3/retweeted_by.json', 'GET')
-  });
-
-  test('show | with multiple contexts', function() {
-    api.show('sentences', { context: 'goal,item' });
-    api.getSentences({ goal_id: 910, item_id: 3350 });
-    assertRouteCalled(api, 'http://domain/goals/910/items/3350/sentences.json', 'GET')
-  });
-
-  test('show | with context and special pluralize', function() {
-    api.show('sentences', { context: 'goal', plural: 'goalsies' });
-    api.getSentences({ goal_id: 910 });
-    assertRouteCalled(api, 'http://domain/goalsies/910/sentences.json', 'GET')
-  });
-
-  test('show | with context and plural is false', function() {
-    api.show('sentences', { context: 'goal', plural: false });
-    api.getSentences({ goal_id: 910 });
-    assertRouteCalled(api, 'http://domain/goal/910/sentences.json', 'GET')
-  });
-
-  test('show | with context and plural is blank', function() {
-    api.show('sentences', { context: 'goal', plural: '' });
-    api.getSentences({ goal_id: 910 });
-    assertRouteCalled(api, 'http://domain/goal/910/sentences.json', 'GET')
-  });
-
-  test('show | with multiple contexts with different pluralize strategies', function() {
-    api.show('sentences', { context: [{ text: 'goal', plural: 'poops' }, { text: 'item' }] });
-    api.getSentences({ goal_id: 910, item_id: 3350 });
-    assertRouteCalled(api, 'http://domain/poops/910/items/3350/sentences.json', 'GET')
-  });
-
-  test('show | with id', function() {
-    api.show('retweets');
-    api.getRetweets({ id: 3 });
-    assertRouteCalled(api, 'http://domain/retweets/3.json', 'GET')
-  });
-
-  test('index | without id', function() {
-    api.index('retweets');
-    api.getRetweets();
-    assertRouteCalled(api, 'http://domain/retweets.json', 'GET')
-  });
-
-  test('index | with id', function() {
-    api.index('retweets');
-    api.getRetweets({ id: 3 });
-    assertRouteCalled(api, 'http://domain/retweets.json', 'GET', { id: 3 })
-  });
-
-  test('create', function() {
-    api.create('tweet');
-    api.createTweet();
-    assertRouteCalled(api, 'http://domain/tweets.json', 'POST')
-  });
-
-
-
-
-  test('resource | defaults', function() {
-    api.resource('cat');
-    equal(typeof api.getCats, 'function', 'get exists');
-    equal(typeof api.getCat, 'function', 'get exists');
-    equal(typeof api.createCat, 'function', 'create exists');
-    equal(typeof api.updateCat, 'function', 'update exists');
-    equal(typeof api.destroyCat, 'function', 'delete exists');
-  });
-
-  test('resource | with context', function() {
-    api.resource('cat');
-
-    api.getCats();
-    assertRouteCalled(api, 'http://domain/cats.json', 'GET')
-    api.getCat({ id: 3 });
-    assertRouteCalled(api, 'http://domain/cats/3.json', 'GET')
-    api.createCat();
-    assertRouteCalled(api, 'http://domain/cats.json', 'POST')
-    api.updateCat({ id: 3 });
-    assertRouteCalled(api, 'http://domain/cats/3.json', 'PUT')
-    api.destroyCat({ id: 3 });
-    assertRouteCalled(api, 'http://domain/cats/3.json', 'DELETE')
-  });
-
-
-  /*
-  test('Resources | defaults', function() {
-    api.resources('cat');
-    equal(typeof api.getCats, 'function', 'get exists');
-    equal(typeof api.createCat, 'function', 'create exists');
-    equal(typeof api.updateCat, 'function', 'update exists');
-    equal(typeof api.destroyCat, 'function', 'delete exists');
-  });
-
-
-
-
-
-
-  /*
-  var setup_methods = ['domain'], index = 0, method, name;
-
-  var api = new APIInterface();
-
-  while(index < setup_methods.length) {
-
-    method = setup_methods[index];
-    name = 'APIInterface#' + method;
-    test(name, function() {
-      ok(api[method], name + ' should exist');
-      strictEqual(api[method]('test'), api, name + ' setting should return the instance');
-      ok(api[method](), 'test', name + ' calling without arguments should return the field');
-    });
-    index++;
-  }
-
-  */
 
 })();
